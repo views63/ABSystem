f17b74eeb98aef1857ccb1636ffd87e3c323bdae
idle <idle@local.net>
2017-12-30 09:32:47 +0800

    add Unity2017 support

--- Assets/ABSystem/Editor/AssetBundleBuildPanel.cs
+++ Assets/ABSystem/Editor/AssetBundleBuildPanel.cs
@@ -21,7 +21,7 @@ namespace Tangzx.ABSystem
             if (config == null)
                 return;
 
-#if UNITY_5
+#if UNITY_5 || UNITY_2017_1_OR_NEWER
 			ABBuilder builder = new AssetBundleBuilder5x(new AssetBundlePathResolver());
 #else
 			ABBuilder builder = new AssetBundleBuilder4x(new AssetBundlePathResolver());
@@ -43,10 +43,10 @@ namespace Tangzx.ABSystem
 
 		static T LoadAssetAtPath<T>(string path) where T:Object
 		{
-#if UNITY_5
-			return AssetDatabase.LoadAssetAtPath<T>(savePath);
+#if UNITY_5 || UNITY_2017_1_OR_NEWER
+			return AssetDatabase.LoadAssetAtPath<T>(path);
 #else
-			return (T)AssetDatabase.LoadAssetAtPath(savePath, typeof(T));
+			return (T)AssetDatabase.LoadAssetAtPath(path, typeof(T));
 #endif
 		}
 
